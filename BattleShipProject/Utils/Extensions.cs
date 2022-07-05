d64edf1ec7761dcc32aac3bb2264681101e2a1d9
rohithakim <rohithakim@gmail.com>
2022-07-05 19:34:12 +1000

    Add files via upload

---
+++ BattleShipProject/Utils/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleShipProject.Entities;
+
+namespace BattleShipProject.Utils
+{
+    public static class PanelExtensions
+    {
+        public static Panel At(this List<Panel> panels, int row, int column)
+        {
+            return panels.Where(x => x.Row == row && x.Column == column).First();
+        }
+    }
+}
---
+++ BattleShipProject/Utils/OccupationStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipProject.Utils
+{
+    public enum OccupationStatus
+    {
+        Empty = 'o',
+        Battleship = 'B',
+        Hit = 'X',
+        Miss = 'M',
+        Destroyed = 'D'
+    }
+
+    public enum ShotResult
+    {
+        Hit,
+        Miss
+    }
+}
